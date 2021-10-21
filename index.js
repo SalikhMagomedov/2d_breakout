88a7d5fe88a8f1902fed7d5c673b99fea6bdd1a5
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-10-21 15:12:27 +0300

    Bricks

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const paddleHeight = 10;
 const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+
 let rightPressed = false;
 let leftPressed = false;
 
@@ -48,11 +58,29 @@ const drawPaddle = () => {
     ctx.closePath();
 };
 
+const drawBricks = () => {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+            const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = '#0095FF';
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawBall();
     drawPaddle();
+    drawBricks();
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -88,5 +116,12 @@ const draw = () => {
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
 
 const interval = setInterval(draw, 10);
